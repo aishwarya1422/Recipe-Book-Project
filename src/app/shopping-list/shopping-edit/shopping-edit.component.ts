7c52e8f98ccaed30eefe1936c873c1a093304018
aishwarya1422 <aishwaryajain1422@gmail.com>
2020-02-19 15:58:12 +0530

    5 files modified

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef,ViewChild,Output,EventEmitter} from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector : 'shopping-edit',
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
 })
 
 export class ShoppingEditComponent {
+  @ViewChild('nameInput',{static : false}) nameInputRef : ElementRef;
+  @ViewChild('amountInput',{static : false}) amountInputRef : ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
+  onAddItem(){
+    const newIngredient = new Ingredient(this.nameInputRef.nativeElement.value,this.amountInputRef.nativeElement.value);
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
