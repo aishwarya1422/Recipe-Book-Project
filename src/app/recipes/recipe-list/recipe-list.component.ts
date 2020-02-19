61a5117fb63213b0c2b900c53e57c195ebee8859
aishwarya1422 <aishwaryajain1422@gmail.com>
2020-02-19 15:16:26 +0530

    8 files modified

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,OnInit,EventEmitter,Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,18 @@ import { Recipe } from '../recipe.model';
 })
 
 export class RecipeListComponent implements OnInit{ 
+ @Output() recipeWasSelected = new EventEmitter<Recipe>();
 recipes:Recipe[] = [  
-  new Recipe('A test Recipe', 'This is a test recipe','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBiy2LHwQyiRBUl2JtA9CAR3ffGmVmu7OvhNjDWtbacLlGEL6I&s'),
-  new Recipe('A test Recipe', 'This is a test recipe','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBiy2LHwQyiRBUl2JtA9CAR3ffGmVmu7OvhNjDWtbacLlGEL6I&s')
+  new Recipe('Pasta', 'This is Red penne pasta , a well known Italian dish','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBiy2LHwQyiRBUl2JtA9CAR3ffGmVmu7OvhNjDWtbacLlGEL6I&s'),
+  new Recipe('Pizza', 'Most delicious junk food','https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-1.2.1&w=1000&q=80')
 ];
 
 constructor(){
 
+}
+
+onSelectedRecipe(recipeSelected:Recipe){
+  this.recipeWasSelected.emit(recipeSelected);
 }
   ngOnInit(){
 
