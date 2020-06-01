f2a193d64ef9db5ae2d9a7eedb2395c94eb3ac76
aishwarya1422 <aishwaryajain1422@gmail.com>
2020-06-01 16:29:24 +0530

    2 files modified, 1 added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,26 +1,26 @@
 import { Component,OnInit,EventEmitter,Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { recipeService } from '../recipe.service';
 
 @Component({
   selector:'recipe-list',
   templateUrl:'./recipe-list.component.html'
+
 })
 
 export class RecipeListComponent implements OnInit{ 
  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-recipes:Recipe[] = [  
-  new Recipe('Pasta', 'This is Red penne pasta , a well known Italian dish','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBiy2LHwQyiRBUl2JtA9CAR3ffGmVmu7OvhNjDWtbacLlGEL6I&s'),
-  new Recipe('Pizza', 'Most delicious junk food','https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-1.2.1&w=1000&q=80')
-];
+  recipes:Recipe[] = [];
 
-constructor(){
+constructor(private recipeService : recipeService){
 
+}
+ngOnInit(){
+ this.recipeService.getRecipes();
 }
 
 onSelectedRecipe(recipeSelected:Recipe){
   this.recipeWasSelected.emit(recipeSelected);
 }
-  ngOnInit(){
-
-  }
+  
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { recipeService } from './recipe.service';
 
 @Component({
   selector:'app-recipes',
   templateUrl:'./recipes.component.html',
-  styleUrls:['./recipes.component.css']
+  styleUrls:['./recipes.component.css'],
+  providers:[recipeService]
 })
 export class RecipeComponent{
   selectedRecipe:Recipe;
