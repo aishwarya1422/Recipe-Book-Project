6731ca0ada45f2fa2cb6cb0a07fb58cd3dacd8f8
aishwarya1422 <aishwaryajain1422@gmail.com>
2020-08-06 22:39:30 +0530

    4 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from "@angular/core";
 import { RecipeComponent } from './recipes/recipes.component';
 import { Routes,RouterModule } from "@angular/router";
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeItemComponent } from 
+'./recipes/recipe-list/recipe-item/recipe-item.component';
 
 const appRoutes : Routes = [
   {path: '', pathMatch: 'full', redirectTo: '/recipes'},
-  {path: 'recipes', component: RecipeComponent},
+  {path: 'recipes', component: RecipeComponent,children:[
+    {path : 'recipeItem', component: RecipeItemComponent }
+    ]},
   {path: 'shoppingList', component: ShoppingListComponent}
 ];
 
